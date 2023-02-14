4e29de773ab69b1d11904db353f45064e659de28
RTS-1989 <63114573+RTS-1989@users.noreply.github.com>
2023-02-14 14:22:16 +0500

    Minor fix in config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -12,7 +12,7 @@ type Config struct {
 func LoadConfig() (Config, error) {
 	var c Config
 	viper.AddConfigPath("./pkg/config/envs")
-	viper.AddConfigPath("/GoAuthSvc") //Для docker
+	viper.AddConfigPath("/GoCensorSvc") //Для docker
 
 	viper.SetConfigName("prod")
 	viper.SetConfigType("env")
